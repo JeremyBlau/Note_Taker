6ec5e80587effbdddf7c93a92de936d910f0524f
jwblau2 <jeremy@abundantly.com>
2023-09-07 19:11:50 -0600

    added code to setup middleware

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public')); 
