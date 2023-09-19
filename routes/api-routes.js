f38857754a5ee04ef454ec6e048133885c84f3ae
jwblau2 <jeremy@abundantly.com>
2023-09-18 20:23:39 -0600

    added necessary code to api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,59 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+// Define the absolute path to your db.json file
+const dbFilePath = path.join(__dirname, 'db', 'db.json');
+
 // Read notes from db.json
-app.get('/api/notes', (req, res) => {
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+router.get('/api/notes', (req, res) => {
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while reading notes.' });
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+// Save a new note to db.json
+router.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while reading notes.' });
+    }
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
       if (err) {
         console.error(err);
-        return res.status(500).json({ error: 'An error occurred while reading notes.' });
+        return res.status(500).json({ error: 'An error occurred while saving the note.' });
       }
-      const notes = JSON.parse(data);
-      res.json(notes);
+      res.json(newNote);
     });
   });
-  
-  // Save a new note to db.json
-  app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuidv4();
-  
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+});
+
+// Handle deleting a note by ID
+router.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while reading notes.' });
+    }
+    const notes = JSON.parse(data);
+
+    // Filter out the note to be deleted by ID
+    const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile(dbFilePath, JSON.stringify(updatedNotes), (err) => {
       if (err) {
         console.error(err);
-        return res.status(500).json({ error: 'An error occurred while reading notes.' });
+        return res.status(500).json({ error: 'An error occurred while deleting the note.' });
       }
-      const notes = JSON.parse(data);
-      notes.push(newNote);
-  
-      fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: 'An error occurred while saving the note.' });
-        }
-        res.json(newNote);
-      });
+      res.json({ message: 'Note deleted successfully' });
     });
   });
-  
-  // Handle deleting a note by ID
-  app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-  
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'An error occurred while reading notes.' });
-      }
-      const notes = JSON.parse(data);
-  
-      // Filter out the note to be deleted by ID
-      const updatedNotes = notes.filter((note) => note.id !== noteId);
-  
-      fs.writeFile(dbFilePath, JSON.stringify(updatedNotes), (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: 'An error occurred while deleting the note.' });
-        }
-        res.json({ message: 'Note deleted successfully' });
-      });
-    });
-  });
-  
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const html_routes = require('/routes/html-routes')
-const api_routes = require('/routes/api-routes')
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const html_routes = require('./routes/html-routes')
+const api_routes = require('./routes/api-routes')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
