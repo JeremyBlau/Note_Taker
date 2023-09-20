3c58d231ba3991e9319365b29a64ca52ca4fc60d
jwblau2 <jeremy@abundantly.com>
2023-09-19 20:04:01 -0600

    added js to the server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const htmlRoutes = require('./public/routes/html_routes');
+const apiRoutes = require('./public/routes/api_routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware for parsing JSON and URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Use the HTML and API routes
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
